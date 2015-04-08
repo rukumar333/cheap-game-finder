0de7aab742bfe04312c48858051430d8cc904226
= <jterner1@binghamton.edu>
2015-04-08 15:59:35 -0400

    This code has a stupid parse error that I don't understand

--- xmltest.hs
+++ xmltest.hs
@@ -7,13 +7,19 @@ import Text.XML.HXT.Core
 
 
 data Game = Game {
-		gameId :: Int,
+		gameId :: String,
 		gameTitle :: String,
-		releaseYear :: Int,
+		releaseYear :: String,
 		platform :: String
 		} deriving (Show)
 
 
+getGame = deep (isElem >>> hasName "Game") >>> proc x -> do
+			id' <- getText <<< getChildren <<< deep (hasName "id") -< x
+			title <- getText <<< getChildren <<< deep (hasName "GameTitle") -< x
+			year <- getText <<< getChildren <<< deep (hasName "ReleaseDate") -< x
+			platform' <- getText <<< getChildren <<< deep (hasName "Platform") -< x 
+			returnA -< Game {gameId = id', gameTitle = title, releaseYear = year, platform = platform'}
 
 
 

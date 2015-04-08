e825ed2a2a8bc9c5d0fafc7d9dbb1c3624f228e3
= <jterner1@binghamton.edu>
2015-04-08 15:39:25 -0400

    an xml file

--- xmltest.hs
+++ xmltest.hs
@@ -2,3 +2,19 @@
 --description: I'm testing the parsing of xml from the game database
 
 
+--this is code from the wiki
+import Text.XML.HXT.Core
+
+
+data Game = Game {
+		gameId :: Int,
+		gameTitle :: String,
+		releaseYear :: Int,
+		platform :: String
+		} deriving (Show)
+
+
+
+
+
+

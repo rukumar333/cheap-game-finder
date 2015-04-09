a919f89898417e5ec89c7c210e2b5079198d37ad
= <jterner1@binghamton.edu>
2015-04-09 10:31:17 -0400

    given a game id (an int) I can get the game, including the cover art

--- gamesdata/GameGet.hs
+++ gamesdata/GameGet.hs
@@ -1,3 +1,8 @@
+--author: Jonathan Terner
+--description: I'm testing the parsing of xml from the game database
+
+
+--this is code from the wiki
 {-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -6,24 +11,60 @@ import Text.XML.HXT.Core
 import Control.Applicative
 import Network.Curl
 import Data.Functor
-import Data.Monoid
-
+import Control.Monad
 
 data Game = Game {
-	gameid :: Int,
-	title :: String,
-	platform :: String,
-	coverArtUrl :: String,
-	} deriving (Show)
-	
+		gameId :: Int,
+		gameTitle :: String,
+		releaseYear :: String,
+		platform :: String,
+		url :: String
+		} deriving (Show)
+
+data ImgUrl = ImgUrl [String]
+
+
 atTag tag = deep (isElem >>> hasName tag)
 text = getChildren >>> getText
 
-getGame = atTag "Game" >>>
+parseXML file = readDocument [ withValidate no
+                             , withRemoveWS yes  -- throw away formating WS
+                             ] file
+
+
+
+getGame = atTag "Data" >>>
+	proc l -> do
+		baseUrl <- getBaseUrl -< l
+		game <- atTag "Game" -< l
+		gameId' <- text <<< atTag "id" -< game
+		gameTitle' <- text <<< atTag "GameTitle" -< game
+		releaseYear' <- text <<< atTag "ReleaseDate" -< game
+		platform' <- text <<< atTag "Platform" -< game
+		returnA -< [gameId', gameTitle',releaseYear',platform',baseUrl]
+
+
+
+getArt = atTag "boxart" >>>
+	proc l -> do
+		front <- getAttrValue "thumb" -< l
+		returnA -< front
+
+getBaseUrl = atTag "Data" >>>
 	proc x -> do
-		gameId' <- text <<< atTag "id" -< x
-		gameTitle' <- text <<< atTag "GameTitle" -< x
-		platform' <- text <<< atTag "Platform" -< x
-		baseUrl <- text <<< atTag "baseImgUrl" -< x
-		ftArt <- text <<< atTag "boxart
-		returnA -< Game {gameId = (read gameId'::Int), title = gameTitle', platform = platform', coverArtUrl = baseUrl <> }
+		url <- text <<< atTag "baseImgUrl" -< x
+		returnA -< url
+		
+
+main = do
+  putStrLn "Enter an id"
+  x <- getLine
+  response <- snd <$> curlGetString ("http://thegamesdb.net/api/GetGame.php?id="++x) []
+  writeFile "text.xml" response
+  games' <- runX (parseXML "text.xml" 
+                    >>> getGame)
+  let games = head games'
+  art <- runX (parseXML "text.xml" >>> getArt)
+  let fullurl = (last $ games) ++ (last art)
+  let game = Game {gameId = ( read (games !! 0)::Int), gameTitle = (games !! 1), releaseYear = (games !! 2), platform = (games !! 3), url = fullurl}
+  print game

e64da63f89c8b49e6a27a014a3ecc3ac9f182a55
Rushil <rukumar333@gmail.com>
2015-04-20 11:07:15 -0400

    css and js files now load when hosted

--- website/Main.hs
+++ website/Main.hs
@@ -9,5 +9,8 @@ import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
-         
+         get "/css/index.css" $ file "css/index.css"
+         get "/bootstrap/css/bootstrap.min.css" $ file "bootstrap/css/bootstrap.min.css"
+         get "/bootstrap/js/bootstrap.min.js" $ file "bootstrap/js/bootstrap.min.js"
+         get "/res/background.jpg" $ file "res/background.jpg"
          get "/" $ file "./index.html"

962217b8b55b5674d2fd6311ee654e73799bc7a1
Rushil <rukumar333@gmail.com>
2015-04-24 02:36:17 -0400

    best buy handles creating propery api queries

--- gameprices/Main.hs
+++ gameprices/Main.hs
@@ -33,20 +33,38 @@ instance FromJSON Game where
                            <*> v .: "platform"
 
 instance Show Game where
-    show (Game na pr pl) = "Game--- Name: " ++ na ++ ", Price: " ++ (show pr) ++ ", Platform: " ++ pl
+    show (Game na pr pl) = "Game -> Name: " ++ na ++ ", Price: " ++ (show pr) ++ ", Platform: " ++ pl
 
 instance Show BestBuyList where
     show (BestBuyList gs pr) = "Got list"
 
+createBestBuyQuery :: String -> String -> String
+createBestBuyQuery name platform = "(" ++ (createSearchQuery name') ++ "&platform=" ++ (createPlatformQuery platform') ++ ")"
+    where name'     = words name
+          platform' = words platform
+
+createSearchQuery :: [String] -> String
+createSearchQuery title = tail $ foldr (\x y -> "&search=" ++ x ++ y) [] title
+
+createPlatformQuery :: [String] -> String
+createPlatformQuery platform | length platform == 1 = head platform
+                             | otherwise            = take (length resultString - 3) resultString
+                             where resultString = foldr (\x y -> x ++ "%20" ++ y) [] platform
+
+-- getBestBuyList :: String -> String -> IO ()
+-- getBestBuyList name platform = do
+  
+
 
 main = do
-  response <- snd <$> curlGetString "http://api.remix.bestbuy.com/v1/products(search=dragon&search=age&search=inquisition&platform=playstation%204)?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh" []
+  name <- getLine 
+  platform <- getLine
+  query <- return $ createBestBuyQuery name platform
+  -- response <- snd <$> curlGetString "http://api.remix.bestbuy.com/v1/products(search=dragon&search=age&search=inquisition&platform=playstation%204)?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh" []
+  response <- snd <$> curlGetString ("http://api.remix.bestbuy.com/v1/products" ++ query ++ "?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh") []
   print response
   let games = decode (fromString response)
-  -- print games
-  -- print . fromJust $ (games :: Maybe BestBuyList)
   print . products $ fromJust $ (games :: Maybe BestBuyList)
-  -- print . fromJust $ ( :: Maybe [Game])
 
 
 -- curl 'http://api.remix.bestbuy.com/v1/products(search=dragon&search=age&search=inquisition&platform=playstation*)?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh'

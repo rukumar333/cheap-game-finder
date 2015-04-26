cef031661bfdc7bf40c5bddb72e839f76f74cbff
Rushil <rukumar333@gmail.com>
2015-04-25 21:10:02 -0400

    included walmart api

--- gameprices/Main.hs
+++ gameprices/Main.hs
@@ -18,7 +18,20 @@ data Game = Game
     {
       name :: String,
       price :: Double,
-      platform :: String
+      platform :: String,
+      productUrl :: String
+    }
+
+data WalmartList = WalmartList
+    {
+      items :: [WalmartGame]
+    }
+
+data WalmartGame = WalmartGame
+    {
+      name' :: String,
+      price' :: Double,
+      productUrl' :: String
     }
 
 instance FromJSON BestBuyList where
@@ -31,40 +44,59 @@ instance FromJSON Game where
                            <$> v .: "name"
                            <*> v .: "salePrice"
                            <*> v .: "platform"
+                           <*> v .: "url"
+
+instance FromJSON WalmartList where
+    parseJSON (Object v) = WalmartList
+                           <$> v .: "items"
+
+instance FromJSON WalmartGame where
+    parseJSON (Object v) = WalmartGame
+                           <$> v .: "name"
+                           <*> v .: "salePrice"
+                           <*> v .: "productUrl"
+                           
 
 instance Show Game where
-    show (Game na pr pl) = "Game -> Name: " ++ na ++ ", Price: " ++ (show pr) ++ ", Platform: " ++ pl
+    show (Game na pr pl ul) = "Game -> Name: " ++ na ++ ", Price: " ++ (show pr) ++ ", Platform: " ++ pl ++ ", url: " ++ ul
+
+instance Show WalmartGame where
+    show (WalmartGame na pr ul) = "Game -> Name: " ++ na ++ ", Price: " ++ (show pr) ++ ", " ++ ul
 
 instance Show BestBuyList where
     show (BestBuyList gs pr) = "Got list"
 
 createBestBuyQuery :: String -> String -> String
-createBestBuyQuery name platform = "(" ++ (createSearchQuery name') ++ "&platform=" ++ (createPlatformQuery platform') ++ ")"
+createBestBuyQuery name platform = "(" ++ (createSearchQuery name') ++ "&platform=" ++ (createSearchQuery platform') ++ ")"
     where name'     = words name
           platform' = words platform
 
 createSearchQuery :: [String] -> String
 createSearchQuery title = tail $ foldr (\x y -> "&search=" ++ x ++ y) [] title
 
-createPlatformQuery :: [String] -> String
-createPlatformQuery platform | length platform == 1 = head platform
+createSpaceQuery :: [String] -> String
+createSpaceQuery platform | length platform == 1 = head platform
                              | otherwise            = take (length resultString - 3) resultString
                              where resultString = foldr (\x y -> x ++ "%20" ++ y) [] platform
 
 -- getBestBuyList :: String -> String -> IO ()
 -- getBestBuyList name platform = do
-  
+
 
 
 main = do
-  name <- getLine 
-  platform <- getLine
-  query <- return $ createBestBuyQuery name platform
-  -- response <- snd <$> curlGetString "http://api.remix.bestbuy.com/v1/products(search=dragon&search=age&search=inquisition&platform=playstation%204)?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh" []
-  response <- snd <$> curlGetString ("http://api.remix.bestbuy.com/v1/products" ++ query ++ "?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh") []
-  print response
-  let games = decode (fromString response)
-  print . products $ fromJust $ (games :: Maybe BestBuyList)
+  -- Best Buy -- 
+  -- name <- getLine 
+  -- platform <- getLine
+  -- query <- return $ createBestBuyQuery name platform
+  -- response <- snd <$> curlGetString ("http://api.remix.bestbuy.com/v1/products" ++ query ++ "?show=name,salePrice,platform,url&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh") []                
+  -- let games = decode (fromString response)
+  -- print . products $ fromJust $ (games :: Maybe BestBuyList)
 
+  -- Walmart -- 
+  name <- getLine
+  query <- return $ createSpaceQuery (words name)
+  response <- snd <$> curlGetString ("http://api.walmartlabs.com/v1/search?apiKey=cb6xua2avdqjj4ck26zry2jh&query=" ++ query) []
+  let games' = decode (fromString response)
+  print . items $ fromJust $ (games' :: Maybe WalmartList)
 
--- curl 'http://api.remix.bestbuy.com/v1/products(search=dragon&search=age&search=inquisition&platform=playstation*)?show=name,salePrice,platform&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh'

cd849c16946b8f692964039d07d1e8cf9dd97927
Rushil <rukumar333@gmail.com>
2015-04-29 10:54:13 -0400

    cant get website stuff to work

--- website/Main.hs
+++ website/Main.hs
@@ -4,12 +4,17 @@
 import Lucid
 import Lucid.Base
 import Data.Monoid
+import Data.Text.Internal
 import Control.Applicative
+import Control.Monad
+import Data.String
 import Web.Scotty
-import qualified Walmart as W 
+import qualified Prices.Walmart as W 
+import qualified Prices.BestBuy as B
 
 main :: IO ()
 main = scotty 3000 $ do
+         -- scriptFile <- readFile "/bootstrap/js/bootstrap.min.js"
          get "/css/index.css" $ file "css/index.css"
          get "/bootstrap/css/bootstrap.min.css" $ file "bootstrap/css/bootstrap.min.css"
          get "/bootstrap/js/bootstrap.min.js" $ file "bootstrap/js/bootstrap.min.js"
@@ -19,9 +24,30 @@ main = scotty 3000 $ do
          get "/search" $
              
              html . renderText $
-                  html_ $ do 
+             -- doctype_ html_ $ do
+                  html_ [lang_ "en"] $ do 
+                       head_ $ do
+                         meta_ [charset_ "utf-8"]
+                         meta_ [httpEquiv_ "X-UA-Compatible", content_ "IE-edge"]
+                         title_ "Game Finder"
+                         link_ [href_ "bootstrap/css/bootstrap.min.css", rel_ "stylsheet", type_ "text/css"]
+                         link_ [href_ "css/index.css", rel_ "stylsheet", type_ "text/css"]
                        body_ $ do
-                            h1_ "Submitted data"
+                         -- h1_ "Submitted data"
+                         nav_ [class_ "navbar navbar-inverse"] $ do
+                                         div_ [class_ "container-fluid"] $ do
+                                                                   div_ [class_ "navbar-header"] $ do
+                                                                             a_ [class_ "navbar-brand", href_ "#"] "Cheap Game Finder"
+                         div_ [class_ "background"] ""
+                         -- script_ $ src_ "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"
+                         -- with script_ [src_ "/bootstrap/js/bootstrap.min.js"] 
+                         -- scriptFile <- readFile "/bootstrap/js/bootstrap.min.js"
+                         script_ $ do
+                                 -- scriptFile <- readFile "/bootstrap/js/bootstrap.min.js"
+                                 fromString scriptFile
+                         -- h1_ "Hello"
+                         -- print scriptFile
+                         -- script_ (fromString scriptFile)
 
          
                      
---
+++ website/Prices/BestBuy.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Prices.BestBuy where
+
+import Data.Aeson
+import Data.Functor
+import Data.Maybe
+import Data.String
+import Network.Curl
+import Control.Applicative
+import Control.Monad
+
+data BestBuyList = BestBuyList
+    { 
+      total :: Int,     
+      products :: [Game]
+    }
+                   deriving (Show)
+
+data Game = Game
+    {
+      name :: String,
+      price :: Double,
+      platform :: String,
+      productUrl :: String
+    }
+            deriving (Show)
+
+
+instance FromJSON BestBuyList where
+    parseJSON (Object v) = BestBuyList 
+                           <$> v .: "total"
+                           <*> v .: "products"
+
+instance FromJSON Game where
+    parseJSON (Object v) = Game 
+                           <$> v .: "name"
+                           <*> v .: "salePrice"
+                           <*> v .: "platform"
+                           <*> v .: "url"
+
+createBestBuyQuery :: String -> String -> String
+createBestBuyQuery name platform = "(" ++ (createSearchQuery name') ++ "&platform=" ++ (createSpaceQuery platform') ++ ")"
+    where name'     = words name
+          platform' = words platform
+
+createSearchQuery :: [String] -> String
+createSearchQuery title = tail $ foldr (\x y -> "&search=" ++ x ++ y) [] title
+
+createSpaceQuery :: [String] -> String
+createSpaceQuery platform | length platform == 1 = head platform
+                             | otherwise            = take (length resultString - 3) resultString
+                             where resultString = foldr (\x y -> x ++ "%20" ++ y) [] platform
+
+
+getBestBuy name platform = do
+  query <- return $ createBestBuyQuery name platform           
+  response <- snd <$> curlGetString ("http://api.remix.bestbuy.com/v1/products" ++ query ++ "?show=name,salePrice,platform,url&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh") []                
+  let games = decode (fromString response)
+  return $ fromJust (games :: Maybe BestBuyList)
+
+main = do
+  -- Best Buy -- 
+  name <- getLine 
+  platform <- getLine
+  query <- return $ createBestBuyQuery name platform
+  response <- snd <$> curlGetString ("http://api.remix.bestbuy.com/v1/products" ++ query ++ "?show=name,salePrice,platform,url&format=json&apiKey=xdapygd5t8dwnbbn5653h9jh") []                
+  let games = decode (fromString response)
+  print . products $ fromJust $ (games :: Maybe BestBuyList)
+
+

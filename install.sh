4cbe88692b9d572f18a837510956ce978b299b4d
Jon <jterner1@binghamton.edu>
2015-05-02 22:33:17 -0400

    yeah

--- install.sh
+++ install.sh
@@ -9,5 +9,7 @@ sqlite3 games.db "CREATE TABLE xboxOne_games (id INTEGER PRIMARY KEY, gameId INT
 sqlite3 games.db "CREATE TABLE wii_games (id INTEGER PRIMARY KEY, gameId INTEGER, title TEXT, year TEXT, platform TEXT, url TEXT, price REAL);"
 sqlite3 games.db "CREATE TABLE wiiU_games (id INTEGER PRIMARY KEY, gameId INTEGER, title TEXT, year TEXT, platform TEXT, url TEXT, price REAL);"
 sqlite3 games.db "CREATE TABLE ids (id INTEGER PRIMARY KEY, appid INTEGER, name TEXT);"
+sqlite3 games.db "CREATE TABLE valid_steam (id INTEGER PRIMARY KEY, name TEXT, price REAL);"
+
 
 

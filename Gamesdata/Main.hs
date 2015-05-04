b82b4ab4b36f0937eafe1b02ba48f804ee62ea07
Jon <jterner1@binghamton.edu>
2015-05-04 16:17:42 -0400

    Preparing supprot for regex database searching

--- Gamesdata/Main.hs
+++ Gamesdata/Main.hs
@@ -11,6 +11,7 @@ import GHC.Int
 import Data.Text
 import Data.Time.Clock
 import Data.Time.Calendar
+import Data.Char (ord)
 
 -- these commands need to be run to set up the data base
 --   sqlite3 gamelist.db "CREATE TABLE pc_games (id INTEGER PRIMARY KEY, gameId INTEGER, title TEXT, year TEXT, platform TEXT, url TEXT, price REAL);"
@@ -68,8 +69,14 @@ getUrl n platform  = do
 
 
  
-
-
---removes games that are older than the specified paramater (in years)
+--creates a regex string to be used when pulling a game from the database
+createDBRegex :: String -> String
+createDBRegex s = let list = Data.String.words (keepLetters s)
+                  in  create 0 (Prelude.length list) list
+  where create :: Int -> Int -> [String] -> String
+	create _  _ [x] = "(" ++ x ++ ")"
+        create index size (x:xs)  | index == (size - 1) = "(" ++ x ++ ")"
+                                  | otherwise = "(" ++ x ++ ").+" ++ create (index + 1) size xs
+        keepLetters string =  Prelude.filter (\x -> (ord x) <= 122) string
 
 	
